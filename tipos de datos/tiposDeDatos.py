4b6feec9c14769ab2adc070d232e34179aa2b59d
juan pablo romero <jpr.1992@hotmail.com>
2023-11-09 18:20:46 -0300

    estudio dia 1

---
+++ tipos de datos/tiposDeDatos.py
@@ -0,0 +1,22 @@
+print('hola mundo')
+
+#Tipos de datos
+#simples:
+#strings simple con comillas
+"string"
+'string'
+#dobles filas con triple comilla
+"""string
+doble fila:
+    doble fila"""
+'''string
+doble fila'''
+
+#INT - nomero entero 
+4
+#FLOAT - numero con coma/decimal
+4.4
+
+#Booleanos  - a diff de js van con mayuscula
+True
+False
---
+++ tipos de datos/variables.py
@@ -0,0 +1,17 @@
+#Variables - recordar usar snake_case
+
+a = 8
+b = 7
+b += 38
+b -= 2
+f = a + b 
+
+# f formatea un tipo de dato a string - funciona como agregar a una cadena ${} en js
+edad_que_tiene = f"Eduardo tiene {f} años "
+
+#del se usa para eliminar variables
+del a
+
+#operadores de pertenencia
+# dato in dato = esta dentro de ? // not in = no esta 
+print("Eduardo" not in edad_que_tiene)
